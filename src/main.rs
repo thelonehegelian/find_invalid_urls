7b36c64df5f422fe86a975b9039eb2142efdf63a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 00:07:21 +0000

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use error_chain::error_chain;
+use reqwest::StatusCode;
+use select::document::Document;
+use select::predicate::Name;
+use std::collections::HashSet;
+use url::{Position, Url};
+
+error_chain! {
+  foreign_links {
+      ReqError(reqwest::Error);
+      IoError(std::io::Error);
+      UrlParseError(url::ParseError);
+      JoinError(tokio::task::JoinError);
+  }
+}
+
 fn main() {
     println!("Hello, world!");
 }
